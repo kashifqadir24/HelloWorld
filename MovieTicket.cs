84a7ee509a6b020513b3c38750dcfd2b2a448aa6
kashifqadir24 <kashifqadir24@gmail.com>
2020-10-04 11:44:51 -0400

    First Commit

---
+++ MovieTicket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/* You want to determine the price of a movie ticket for someone given their age.
+The prices are as follows: 5 and under (free), 5-14 (7.99), 14-65 (11.99), 65+ (9.99) */
+
+namespace HelloWorld
+{
+    class MovieTicket
+    {
+        static void Main(string[] args)
+        {
+            int userAge;
+
+            Console.Write("Please enter your age: ");
+            userAge = Convert.ToInt32(Console.ReadLine());
+
+            if (userAge < 0 || userAge > 100)
+            {
+                Console.WriteLine("Invalid Age, please visit Customer Service");
+            }
+            else if (userAge < 5)
+                Console.WriteLine("Your movie ticket price is $0");
+            else if (userAge < 14)
+                Console.WriteLine("Your movie ticket price is $7.99");
+            else if (userAge < 65)
+                Console.WriteLine("Your movie ticket price is $11.99");
+            else
+            {
+                Console.WriteLine("Your movie ticket price is $9.99");
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
